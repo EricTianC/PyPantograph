a3030a7f7f7867d69f04d176e865b3ca24fec565
EricTian <erictianc@outlook.com>
2024-12-20 00:16:15 +0800

    Update

--- examples/leanf_server.py
+++ examples/leanf_server.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+import secrets
 
 app = FastAPI()
 
@@ -15,7 +16,7 @@ def get_project_and_lean_path():
     p = subprocess.check_output(['lake', 'env', 'printenv', 'LEAN_PATH'], cwd=cwd)
     return cwd, p
 
-def state2json(state: GoalState):
+def state2json(state: GoalState, session: str):
     return {
         "goals": list(map(lambda goal : 
                           {
@@ -29,6 +30,7 @@ def state2json(state: GoalState):
                                 "is_conversion": goal.is_conversion,
                             }, state.goals)),
         "is_solved": state.is_solved,
+        "session": session,
     }
 
 
@@ -43,16 +45,41 @@ state0 = server.goal_start("forall (p q: Prop), Or p q -> Or q p")
 state1 = server.goal_tactic(state0, goal_id=0, tactic="aesop")
 assert state1.is_solved
 
-@app.get("goal_start")
+session_pool = {}
+
+@app.get("/goal_start")
 async def goal_start(prop: str):
-    state0 = server.goal_start(prop)
-    return state2json(state0)
+    session = secrets.token_urlsafe(16)
+    try:
+        state0 = server.goal_start(prop)
+        session_pool[session] = state0
+        return state2json(state0, session=session)
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
     
-@app.get("goal_tactic")
-async def goal_tactic(state, goal_id: int, tactic: str):
-    new_state = server.goal_tactic(state, goal_id, tactic)
-    return state2json(new_state)
+@app.get("/goal_tactic")
+async def goal_tactic(session: str, goal_id: int, tactic: str):
+    try:
+        new_state = server.goal_tactic(session_pool[session], goal_id, tactic)
+        session_pool[session] = new_state
+        return state2json(new_state, session=session)
+    except KeyError:
+        raise HTTPException(status_code=404, detail="Session not found")
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
+
+@app.get("/close_session")
+async def close_session(session: str):
+    try:
+        del session_pool[session]
+        return {"status": "ok"}
+    except KeyError:
+        raise HTTPException(status_code=404, detail="Session not found")
+
+@app.get("/check_session_pool")
+async def check_session_pool():
+    return session_pool
 
 @app.route("/")
-async def test(request):
+async def test():
    return {"hello": "world"}
